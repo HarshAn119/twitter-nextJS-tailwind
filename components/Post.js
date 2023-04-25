65f17a0d802498671f1ba91e03002a86bd11a3d1
Harsh <harshanand119@gmail.com>
2023-04-25 17:54:28 +0530

    get data from firestore

--- components/Post.js
+++ components/Post.js
@@ -1,21 +1,38 @@
-import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, TrashIcon } from "@heroicons/react/outline";
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from '@heroicons/react/outline';
+import Moment from 'react-moment';
 
 export default function Post({ post }) {
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200">
       {/* imgage */}
-      <img className="h-11 w-11 rounded-full mr-4" src={post.userImg} alt="user-img" />
+      <img
+        className="h-11 w-11 rounded-full mr-4"
+        src={post.data().userImg}
+        alt="user-img"
+      />
 
       {/* right side */}
       <div className="">
-
         {/* header */}
         <div className="flex items-center justify-between">
           {/* user info */}
           <div className="flex items-center space-x-1 whitespace-nowrap">
-            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.name}</h4>
-            <span className="text-sm sm:text-[15px] text-gray-500">@{post.username}</span>
-            <span className="text-sm sm:text-[15px] hover:underline">{post.timestamp}</span>
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+              {post.data().name}
+            </h4>
+            <span className="text-sm sm:text-[15px] text-gray-500">
+              @{post.data().username} -{' '}
+            </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+              <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
+            </span>
           </div>
 
           {/* dot icon */}
@@ -23,10 +40,16 @@ export default function Post({ post }) {
         </div>
 
         {/* post text */}
-        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">{post.text}</p>
+        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">
+          {post.data().text}
+        </p>
 
         {/* post img */}
-        <img className="rounded-2xl mr-2" src={post.img} alt="post-img" />
+        <img
+          className="rounded-2xl mr-2"
+          src={post.data().image}
+          alt="post-img"
+        />
 
         {/* icons */}
         <div className="flex justify-between text-gray-500 p-2">
@@ -38,5 +61,5 @@ export default function Post({ post }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
